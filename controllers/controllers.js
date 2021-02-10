8c61a628078868c9d49aba06211937c5f5423940
Valeria Shegolkova <lera.shapranova@yandex.ru>
2021-02-10 23:51:07 +0300

    fix

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -22,7 +22,7 @@ const getCards = (req, res) => getDataFromFile(cardDataPath)
   .then((cards) => res.status(200).send(cards))
   .catch((err) => res.status(500).send(err));
 
-const getError = (req, res) => res.status(404).send({ 'message': 'Запрашиваемый ресурс не найден' });
+const getError = (req, res) => res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
 
 module.exports = {
   getUsers, getProfile, getCards, getError,
