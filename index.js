28d0db79aab46e3a3ec8366e3720c8459138223a
Valeria Shegolkova <lera.shapranova@yandex.ru>
2021-02-10 16:31:00 +0300

    add routes

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const usersRouter = require('./routes/users');
 
 const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', usersRouter);
 
 app.listen(PORT,() =>{
     console.log(`App listening on port ${PORT}`);
 });
-
-  
